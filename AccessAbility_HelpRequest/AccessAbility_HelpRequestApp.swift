493c34583f7a7cbfd94bf9e8cb004349c5ba40fb
arpangtm <gautamarpan8@gmail.com>
2026-03-11 13:43:55 -0500

    Initial Commit

---
+++ AccessAbility_HelpRequest/AccessAbility_HelpRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessAbility_HelpRequestApp.swift
+//  AccessAbility_HelpRequest
+//
+//  Created by Arpan Gautam on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct AccessAbility_HelpRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
